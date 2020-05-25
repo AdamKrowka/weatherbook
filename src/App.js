9182afa3ee99a0a43a2bd8c2751ea0fd9ddc1d4e
Adam96 <adamkrowka108@gmail.com>
2020-05-25 11:44:38 +0200

    App Grid created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Button } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  appWrapper: {
+    boxSizing: "border-box",
+    backgroundColor: "#03a9f4",
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "Roboto",
+    padding: theme.spacing(6),
+    [theme.breakpoints.down("xs")]: {
+      padding: 0,
+    },
+  },
+  innerWrapper: {
+    backgroundColor: "#dff5ff",
+    width: "100%",
+    height: "100%",
+    borderRadius: 20,
+    display: "flex",
+    flexDirection: "column",
+    [theme.breakpoints.down("xs")]: {
+      borderRadius: 0,
+    },
+  },
+  element1: {
+    flexGrow: 0,
+  },
+  element2: {
+    flexGrow: 1,
+  },
+  cityList: {
+    backgroundColor: "white",
+    borderRadius: 20,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.appWrapper}>
+      <div className={classes.innerWrapper}>
+        <div className={classes.element1}>
+          <Button></Button>
+        </div>
+        <Grid container className={classes.element2}>
+          <Grid className={classes.cityList} item xs={3}>
+            City search
+          </Grid>
+          <Grid item xs={9}>
+            City data
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
