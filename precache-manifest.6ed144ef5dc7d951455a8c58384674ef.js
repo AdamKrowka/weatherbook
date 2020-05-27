a447925ec2e2365f0ac461a34fad1a108bff0d21
Adam96 <adamkrowka108@gmail.com>
2020-05-27 16:38:48 +0200

    Updates

---
+++ precache-manifest.6ed144ef5dc7d951455a8c58384674ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ff2c4dd8c3a4a03ab65f78674d78f1",
+    "url": "/weatherbook/index.html"
+  },
+  {
+    "revision": "8b381bc930bf9f6d1d49",
+    "url": "/weatherbook/static/css/main.156b4bfb.chunk.css"
+  },
+  {
+    "revision": "06354cb1d1225749916b",
+    "url": "/weatherbook/static/js/2.e1fa27bf.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/weatherbook/static/js/2.e1fa27bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b381bc930bf9f6d1d49",
+    "url": "/weatherbook/static/js/main.b68147da.chunk.js"
+  },
+  {
+    "revision": "a9fbb67dfc6736582bd8",
+    "url": "/weatherbook/static/js/runtime-main.80daedb0.js"
+  }
+]);
